ed7091954a57d2a7e0a6908870163157722a7d29
Walmyr <wlsf82@gmail.com>
2023-01-15 19:17:10 +0100

    Add new meals

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const meals = [
   {
     name: "Lasanha de berinjela",
-    ingredients: ["berinjela", "molho de tomate", "queijo vegano", "massa de lasanha"],
+    ingredients: ["berinjela", "molho de tomate", "molho branco", "queijo vegano", "massa de lasanha"],
+    type: "hot"
+  },
+  {
+    name: "Espaguete de abobrinha",
+    ingredients: ["abobrinha", "tomate", "manjericão", "azeite", "massa de espaguete"],
+    type: "hot"
+  },
+  {
+    name: "Massa à bolonhesa",
+    ingredients: ["azeite", "massa de espaguete", "molho de tomate", "cebola", "alho"],
+    type: "hot"
+  },
+  {
+    name: "Chilly",
+    ingredients: ["picado carnal", "feijão vermelho", "molho de tomate", "pimenta", "milho", "abacate", "cebola", "tomate", "alho", "pimentão", "nachos"],
+    type: "hot"
+  },
+  {
+    name: "Carreteiro",
+    ingredients: ["proteína", "arroz", "cebola", "alho", "molho de tomate", "tempero verde"],
+    type: "hot"
+  },
+  {
+    name: "Nhoque",
+    ingredients: ["massa de nhoque", "cebola", "alho", "molho vermelho", "pimenta do reino"],
+    type: "hot"
+  },
+  {
+    name: "Strogonoff",
+    ingredients: ["proteína", "arroz", "cebola", "alho", "molho de tomate", "creme vegetal", "batata palha ou frita"],
     type: "hot"
   },
   {
@@ -10,9 +40,34 @@ const meals = [
     type: "salad"
   },
   {
-    name: "Espaguete de abobrinha",
-    ingredients: ["abobrinha", "tomate", "manjericão", "azeite", "massa de espaguete"],
-    type: "hot"
+    name: "Salada de falafel",
+    ingredients: ["falafel", "folhas verdes", "tomate", "azeitonas", "humus"],
+    type: "salad"
+  },
+  {
+    name: "Saladão",
+    ingredients: ["folhas verdes", "tomate", "azeitonas", "milho", "proteína (ex.: Heura)", "crutons", "pimenta"],
+    type: "salad"
+  },
+  {
+    name: "Salada de quinoa e castanhas",
+    ingredients: ["quinoa", "pistacho", "cenoura ralada", "ervilha", "edamame", "gengibre", "agave"],
+    type: "salad"
+  },
+  {
+    name: "Iron Energy",
+    ingredients: ["folhas", "quinoa", "cranberries", "nozes", "uva verde", "molho agridoce", "brocolis", "repolho"],
+    type: "salad"
+  },
+  {
+    name: "Salada de grão de bico",
+    ingredients: ["grão de bico", "cenoura", "pimentão", "coentro", "suco de limão", "pimenta"],
+    type: "salad"
+  },
+  {
+    name: "Ramen",
+    ingredients: ["massa de ramen", "brócolis", "cenoura", "edameme", "tofu", "cebolinha", "misô"],
+    type: "soup"
   },
   {
     name: "Sopa de legumes",
@@ -20,10 +75,75 @@ const meals = [
     type: "soup"
   },
   {
-    name: "Sanduíche de grão-de-bico",
-    ingredients: ["pão integral", "grão-de-bico", "tomate", "alface"],
+    name: "Canja sem galinha",
+    ingredients: ["cenoura", "cebola", "alho", "batata", "arroz", "tempero verde", "proteína (grão de bico ou heura)"],
+    type: "soup"
+  },
+  {
+    name: "Minestrone",
+    ingredients: ["massa para sopa", "feijão", "ervilha", "cenoura", "molho de tomate"],
+    type: "soup"
+  },
+  {
+    name: "Sopa de batata doce cremosa",
+    ingredients: ["batata doce", "leite de côco", "pimenta"],
+    type: "soup"
+  },
+  {
+    name: "Sopa de cenoura cremosa",
+    ingredients: ["cenoura", "cebola", "alho"],
+    type: "soup"
+  },
+  {
+    name: "Sopa de moranaga cremosa",
+    ingredients: ["moranaga", "cebola", "alho"],
+    type: "soup"
+  },
+  {
+    name: "Caldo verde",
+    ingredients: ["espinafre", "brócolis", "ervilha", "cebola", "alho", "couve"],
+    type: "soup"
+  },
+  {
+    name: "Sopa de lentilha",
+    ingredients: ["lentilha", "cenoura", "cebola", "alho", "tempero verde"],
+    type: "soup"
+  },
+  {
+    name: "Sopa cremosa de brócolis",
+    ingredients: ["brócolis", "batata", "cenoura", "ervilha", "creme vegetal", "grão de bico"],
+    type: "soup"
+  },
+  {
+    name: "Sanduíche de milanesa de seitan",
+    ingredients: ["maionese ou humus", "pão integral", "bife à milanesa de seitan", "tomate", "alface"],
     type: "sandwich"
-  }
+  },
+  {
+    name: "Xis Macaco",
+    ingredients: ["pão de sanduíche", "pasta de amendoim", "banana", "canela"],
+    type: "sandwich"
+  },
+  {
+    name: "Subway",
+    ingredients: ["maionese ou humus", "baguete", "tomate", "alface", "cebola roxa", "heura"],
+    type: "sandwich"
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["maionese ou humus", "pão de hamburger", "tomate", "alface", "proteína"],
+    type: "sandwich"
+  },
+  {
+    name: "Cachorro quente",
+    ingredients: ["pão de cachorro quente", "salsicha", "molho de tomate", "milho", "ervilha", "batata palha", "catchup", "mostarda"],
+    type: "sandwich"
+  },
+  {
+    name: "Sanduíche de tomate e pepino",
+    ingredients: ["humus ou margarina", "pão francês", "tomate", "pepino", "pimenta do reino"],
+    type: "sandwich"
+  },
 ];
 
 const mealName = document.getElementById("meal-name");

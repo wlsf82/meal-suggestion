56210d4291b0ebb25c2cf632a51e3814ba43a97a
Walmyr <wlsf82@gmail.com>
2023-01-19 23:32:13 +0100

    Configure ESLint and make it part of CI

--- src/script.js
+++ src/script.js
@@ -5,37 +5,40 @@ const mealEnum = Object.freeze({
   soup: 'sopa'
 })
 
-const mealName = document.getElementById("meal-name");
-const ingredientsLabel = document.getElementById("ingredients-label");
-const ingredientsList = document.getElementById("ingredients-list");
-const generateMealButton = document.getElementById("generate-meal-button");
-const mealTypeFilter = document.getElementById("meal-type-filter");
+const mealName = document.getElementById('meal-name')
+const ingredientsLabel = document.getElementById('ingredients-label')
+const ingredientsList = document.getElementById('ingredients-list')
+const generateMealButton = document.getElementById('generate-meal-button')
+const mealTypeFilter = document.getElementById('meal-type-filter')
 
-let filteredMeals = [...meals];
+// eslint-disable-next-line no-undef
+let filteredMeals = [...meals]
 
-mealTypeFilter.addEventListener("change", e => {
-  const selectedType = e.target.value;
-  if (selectedType === "all") {
-    filteredMeals = [...meals];
+mealTypeFilter.addEventListener('change', e => {
+  const selectedType = e.target.value
+  if (selectedType === 'all') {
+    // eslint-disable-next-line no-undef
+    filteredMeals = [...meals]
   } else {
-    filteredMeals = meals.filter(meal => meal.type === selectedType);
+    // eslint-disable-next-line no-undef
+    filteredMeals = meals.filter(meal => meal.type === selectedType)
   }
-});
+})
 
 function generateMeal() {
-  const randomMeal = filteredMeals[Math.floor(Math.random() * filteredMeals.length)];
-  mealName.innerHTML = `Refeição: ${randomMeal.name} (${mealEnum[randomMeal.type]})`;
-  ingredientsList.innerHTML = "";
+  const randomMeal = filteredMeals[Math.floor(Math.random() * filteredMeals.length)]
+  mealName.innerHTML = `Refeição: ${randomMeal.name} (${mealEnum[randomMeal.type]})`
+  ingredientsList.innerHTML = ''
   for (const ingredient of randomMeal.ingredients) {
-    const listItem = document.createElement("li");
-    ingredientsLabel.innerHTML = "Ingredientes:"
-    listItem.innerHTML = ingredient;
-    ingredientsList.appendChild(listItem);
+    const listItem = document.createElement('li')
+    ingredientsLabel.innerHTML = 'Ingredientes:'
+    listItem.innerHTML = ingredient
+    ingredientsList.appendChild(listItem)
   }
 }
 
 window.onload = () => {
-  generateMeal();
-};
+  generateMeal()
+}
 
-generateMealButton.addEventListener("click", generateMeal);
+generateMealButton.addEventListener('click', generateMeal)

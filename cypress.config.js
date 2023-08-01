c73cd76eccaeb5c3c4640cb0ae313e2a64db4b87
Walmyr <wlsf82@gmail.com>
2023-08-01 14:14:24 +0200

    Filter browsers so electron is not listed

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: 'dyjimv',
-  e2e: {},
+  e2e: {
+    setupNodeEvents(on, config) {
+      return {
+        browsers: config.browsers.filter(browser => browser.name !== 'electron')
+      }
+    }
+  },
   fixturesFolder: false,
 })
